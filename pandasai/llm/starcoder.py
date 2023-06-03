e8970da84ebd207886d389f7f344ff7bd5432ad1
bsvmelo <60415916+bsvmelo@users.noreply.github.com>
2023-06-03 09:21:44 -0400

    Update starcoder.py

--- pandasai/llm/starcoder.py
+++ pandasai/llm/starcoder.py
@@ -30,7 +30,8 @@ class Starcoder(HuggingFaceLLM):
     """
 
     api_token: str
-    _api_url: str = "https://api-inference.huggingface.co/models/bigcode/starcoder"
+    #_api_url: str = "https://api-inference.huggingface.co/models/bigcode/starcoder"
+    _api_url: str = "https://api-inference.huggingface.co/models/google/flan-t5-base"
    # _api_url: str = "https://api-inference.huggingface.co/models/tiiuae/falcon-7b"
     _max_retries: int = 5
 
